3fe36e003e301620f2fe386b6c16a30e8f64788a
Dan Russell <danr94@gmail.com>
2015-10-05 15:27:01 -0700

    Added an interactable projector and a film reel that can be picked up by the player.

---
+++ Source/TheStarscapeShimmer/ProjectorInteract.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "InteractableObject.h"
+#include "ProjectorInteract.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THESTARSCAPESHIMMER_API AProjectorInteract : public AInteractableObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
