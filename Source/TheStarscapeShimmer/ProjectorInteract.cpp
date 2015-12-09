e50475e91db3aa29745c126bd9e9ce244fa95ff5
Dan Russell <danr94@gmail.com>
2015-12-08 17:13:28 -0800

    Puzzle zone is reset after film stops.

--- Source/TheStarscapeShimmer/ProjectorInteract.cpp
+++ Source/TheStarscapeShimmer/ProjectorInteract.cpp
@@ -4,7 +4,7 @@
 #include "FilmReelPickup.h"
 #include "ProjectorInteract.h"
 #include "ProjectorFilmReel.h"
-#include <stdlib.h> 
+#include <stdlib.h>
 
 #define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::White,text)
 
@@ -15,205 +15,211 @@
 //Overrides the interaction method
 void AProjectorInteract::OnInteraction_Implementation(AFirstPersonCharacter* Character)
 {
-	Super::OnInteraction_Implementation(Character);
+    Super::OnInteraction_Implementation(Character);
 
-	// If the projector alread has a reel in it
-	if (CurrentFilmReel != NULL)
-	{
-		// Cannot take it out if you are holding something else
-		if (Character->HeldItem == nullptr)
-		{
-			StopFilm();
-			// Give it to the player to hold.
-			CurrentFilmReel->SetActorScale3D(FVector(3, 3, 3));
-			Character->HeldItem = CurrentFilmReel;
-			CurrentFilmReel = NULL;
-			FilmReelBack->RunReel();
-			FilmReelFront->RunReel();
-		}
-		return;
-	}
+    // If the projector alread has a reel in it
+    if (CurrentFilmReel != NULL)
+    {
+        // Cannot take it out if you are holding something else
+        if (Character->HeldItem == nullptr)
+        {
+            StopFilm();
+            // Give it to the player to hold.
+            CurrentFilmReel->SetActorScale3D(FVector(3, 3, 3));
+            Character->HeldItem = CurrentFilmReel;
+            CurrentFilmReel = NULL;
+            FilmReelBack->RunReel();
+            FilmReelFront->RunReel();
+        }
+        return;
+    }
 
-	if (!HasPower)
-		return;
+    if (!HasPower)
+        return;
 
-	AFilmReelPickup* const Reel = Cast<AFilmReelPickup>(Character->HeldItem);
-	CurrentFilmReel = Reel;
-	
-	// They're not holding a film reel, so do nothing :)
-	if (!Reel) return;
-	UE_LOG(LogTemp, Warning, TEXT("Reel Check"));
-	
+    AFilmReelPickup* const Reel = Cast<AFilmReelPickup>(Character->HeldItem);
+    CurrentFilmReel = Reel;
+
+    // They're not holding a film reel, so do nothing :)
+    if (!Reel) return;
+    UE_LOG(LogTemp, Warning, TEXT("Reel Check"));
+
 #ifdef WITH_EDITOR
-	if (!Reel->Film || !Reel->FilmSound)
-	{
-		FMessageDialog::Debugf(FText::FromString("Trying to use film reel without film or sound. Add a film and its sound to the reel and try again."));
-		return;
-	}
+    if (!Reel->Film || !Reel->FilmSound)
+    {
+        FMessageDialog::Debugf(FText::FromString("Trying to use film reel without film or sound. Add a film and its sound to the reel and try again."));
+        return;
+    }
 #endif // WITH_EDITOR
-	
-	UE_LOG(LogTemp, Warning, TEXT("Attempting pick up %s"), *(Reel->Film->GetName()));
-	// Do some logic here to get the film and play it from da screen
-	this->RunFilm(Reel);
 
-	FilmReelBack->RunReel();
-	FilmReelFront->RunReel();
+    UE_LOG(LogTemp, Warning, TEXT("Attempting pick up %s"), *(Reel->Film->GetName()));
+    // Do some logic here to get the film and play it from da screen
+    this->RunFilm(Reel);
 
-	// Drop the film reel
-	//Character->HeldItem->OnDrop();
-	Character->HeldItem = nullptr;
+    FilmReelBack->RunReel();
+    FilmReelFront->RunReel();
 
-	// Hide the film reel in the projector, lol its a bit hacky
-	CurrentFilmReel->SetActorLocation(this->GetActorLocation());
-	CurrentFilmReel->SetActorScale3D(FVector(0, 0, 0));
+    // Drop the film reel
+    //Character->HeldItem->OnDrop();
+    Character->HeldItem = nullptr;
+
+    // Hide the film reel in the projector, lol its a bit hacky
+    CurrentFilmReel->SetActorLocation(this->GetActorLocation());
+    CurrentFilmReel->SetActorScale3D(FVector(0, 0, 0));
 }
 
 ///<summary>Stops the currently playing film and its audio, if any.</summary>
 void AProjectorInteract::StopFilm()
 {
-	if (CurrentFilmReel == NULL) return;
+    if (CurrentFilmReel == NULL) return;
 
-	TArray<UStaticMeshComponent*> Comps;
+    TArray<UStaticMeshComponent*> Comps;
 
-	TheatreScreen->GetComponents(Comps);
-	if (Comps.Num() > 0)
-	{
-		UStaticMeshComponent* Mesh = Comps[0];
-		Mesh->SetMaterial(0, TheatreScreen->ScreenOffMaterial);
-		CurrentFilmReel->Film->Pause();
-		puzzleZoneStart = 0;
-		puzzleZoneEnd = 0;
-		inPuzzleZone = false;
-		puzzleLocked = false;
-	}
+    TheatreScreen->GetComponents(Comps);
+    if (Comps.Num() > 0)
+    {
+        UStaticMeshComponent* Mesh = Comps[0];
+        Mesh->SetMaterial(0, TheatreScreen->ScreenOffMaterial);
+        CurrentFilmReel->Film->Pause();
+        puzzleZoneStart = 0;
+        puzzleZoneEnd = 0;
+        inPuzzleZone = false;
+        puzzleLocked = false;
+        UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZone")), 0.0f);
+        UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZoneLocked")), 0.0f);
+    }
 
-	UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
-	SpeakerAudio->Stop();
+    UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
+    SpeakerAudio->Stop();
 }
 
 void AProjectorInteract::RunFilm(AFilmReelPickup* Reel)
 {
-	TArray<UStaticMeshComponent*> Comps;
-	
-	TheatreScreen->GetComponents(Comps);
-	if (Comps.Num() > 0)
-	{
-		UStaticMeshComponent* Mesh = Comps[0];
-		Mesh->SetMaterial(0, Reel->FilmMaterial);
-		Reel->Film->Rewind();
-		Reel->Film->Play();
+    TArray<UStaticMeshComponent*> Comps;
 
-		puzzleZoneStart = rand() % (int)(puzzleStartMax) + puzzleStartMin;
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Start: %f"), puzzleZoneStart));
-		puzzleZoneEnd = puzzleZoneStart + puzzleDurationInMinutes;
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("End: %f"), puzzleZoneEnd));
-	}
+    TheatreScreen->GetComponents(Comps);
+    if (Comps.Num() > 0)
+    {
+        UStaticMeshComponent* Mesh = Comps[0];
+        Mesh->SetMaterial(0, Reel->FilmMaterial);
+        Reel->Film->Rewind();
+        Reel->Film->Play();
 
-	UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
-	SpeakerAudio->bStopWhenOwnerDestroyed = false;
-	SpeakerAudio->Stop();
-	SpeakerAudio->SetSound(Reel->FilmSound);
-	SpeakerAudio->Play(0.0f);
+        puzzleZoneStart = rand() % (int)(puzzleStartMax) + puzzleStartMin;
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Start: %f"), puzzleZoneStart));
+        puzzleZoneEnd = puzzleZoneStart + puzzleDurationInMinutes;
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("End: %f"), puzzleZoneEnd));
+    }
+
+    UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
+    SpeakerAudio->bStopWhenOwnerDestroyed = false;
+    SpeakerAudio->Stop();
+    SpeakerAudio->SetSound(Reel->FilmSound);
+    SpeakerAudio->Play(0.0f);
 }
 
 void AProjectorInteract::Tick(float DeltaTime)
 {
-	if (CurrentFilmReel != NULL) {
-		double minutesElapsed = CurrentFilmReel->Film->GetTime().GetTotalMinutes();
-		if (!puzzleLocked && minutesElapsed >= puzzleZoneStart && minutesElapsed <= puzzleZoneEnd) {
-			if (!inPuzzleZone)
-				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("In Puzzle Zone")));
+    if (CurrentFilmReel != NULL) {
+        double minutesElapsed = CurrentFilmReel->Film->GetTime().GetTotalMinutes();
+        if (!puzzleLocked && minutesElapsed >= puzzleZoneStart && minutesElapsed <= puzzleZoneEnd) {
+            if (!inPuzzleZone)
+                GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("In Puzzle Zone")));
 
-			inPuzzleZone = true;
-			UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZone")), 1.0f);
-		}
-		
-		if (puzzleLocked) {
-			if (minutesElapsed > puzzleZoneEnd) {
-				CurrentFilmReel->Film->Seek(FTimespan::FromMinutes(puzzleZoneStart));
-			}
-			else if (minutesElapsed < puzzleZoneStart) {
-				CurrentFilmReel->Film->Seek(FTimespan::FromMinutes(puzzleZoneEnd));
-			}
-		} else if (inPuzzleZone) {
-			if (minutesElapsed > puzzleZoneEnd) {
-				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Out of Puzzle Zone")));
-				UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZone")), 0.0f);
-				inPuzzleZone = false;
-			}
-		}
-	}
+            inPuzzleZone = true;
+            UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZone")), 1.0f);
+        }
+
+        if (puzzleLocked) {
+            if (minutesElapsed > puzzleZoneEnd) {
+                CurrentFilmReel->Film->Seek(FTimespan::FromMinutes(puzzleZoneStart));
+            }
+            else if (minutesElapsed < puzzleZoneStart) {
+                CurrentFilmReel->Film->Seek(FTimespan::FromMinutes(puzzleZoneEnd));
+            }
+        } else if (inPuzzleZone) {
+            if (minutesElapsed > puzzleZoneEnd) {
+                GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Out of Puzzle Zone")));
+                UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZone")), 0.0f);
+                inPuzzleZone = false;
+            }
+        }
+    }
 }
 
-void AProjectorInteract::LockPuzzleZone() 
+void AProjectorInteract::LockPuzzleZone()
 {
-	if (CurrentFilmReel->Film != NULL)
-	{
-		puzzleLocked = true;
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Puzzle Zone Locked")));
-		UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZoneLocked")), 1.0f);
-	}
+    if (CurrentFilmReel->Film != NULL)
+    {
+        puzzleLocked = true;
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Puzzle Zone Locked")));
+        UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZoneLocked")), 1.0f);
+    }
 }
 
 void AProjectorInteract::UnlockPuzzleZone()
 {
-	if (CurrentFilmReel->Film != NULL)
-	{
-		puzzleLocked = false;
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Puzzle Zone Unlocked")));
-		UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZoneLocked")), 0.0f);
-	}
+    if (CurrentFilmReel->Film != NULL)
+    {
+        puzzleLocked = false;
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Puzzle Zone Unlocked")));
+        UKismetMaterialLibrary::SetScalarParameterValue(this, ScreenMatParams, FName(TEXT("PuzzleZoneLocked")), 0.0f);
+    }
 }
 
 void AProjectorInteract::Power_Implementation()
 {
-	HasPower = !HasPower;
+    HasPower = !HasPower;
 }
 
 void AProjectorInteract::ScalePlayRate(float scale) {
-	
-	int playRate = scale * maxPlayRate;
-	if (playRate == 0) playRate = 1.0f;
-	
-	if (CurrentFilmReel != NULL && CurrentFilmReel->Film != NULL && playRate != currentPlayRate) {
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Changed Rate %d"), playRate));
-		currentPlayRate = playRate;
-		CurrentFilmReel->Film->SetRate(playRate);
 
-		/*if (playRate == 1) { // Rate normal again so play audio again
-			TheatreSpeaker->Play(CurrentFilmReel->Film->GetTime().GetTotalSeconds());
-		}*/
-	}
+    int playRate = scale * maxPlayRate;
+    if (playRate == 0) playRate = 1.0f;
+
+    if (CurrentFilmReel != NULL && CurrentFilmReel->Film != NULL && playRate != currentPlayRate) {
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Changed Rate %d"), playRate));
+        currentPlayRate = playRate;
+        CurrentFilmReel->Film->SetRate(playRate);
+
+        UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
+        if (playRate == 1) { // Rate normal again so play audio again
+            SpeakerAudio->Play(CurrentFilmReel->Film->GetTime().GetTotalSeconds());
+        }
+        else {
+            SpeakerAudio->Stop();
+        }
+    }
 }
 
 void AProjectorInteract::FastForward_Implementation()
 {
-	if (HasPower) {
-		CurrentFilmReel->Film->SetRate(16);
-		UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
-		SpeakerAudio->Stop();
-	}
+    if (HasPower) {
+        CurrentFilmReel->Film->SetRate(16);
+        UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
+        SpeakerAudio->Stop();
+    }
 }
 
 void AProjectorInteract::PlayReverse_Implementation()
 {
-	if (HasPower) {
-		CurrentFilmReel->Film->SetRate(-16);
-		UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
-		SpeakerAudio->Stop();
-	}
+    if (HasPower) {
+        CurrentFilmReel->Film->SetRate(-16);
+        UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
+        SpeakerAudio->Stop();
+    }
 }
 
 void AProjectorInteract::PlayNormal_Implementation()
 {
-	if (HasPower) {
-		UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
-		CurrentFilmReel->Film->SetRate(1);
-		SpeakerAudio->Play(CurrentFilmReel->Film->GetTime().GetTotalSeconds());
+    if (HasPower) {
+        UAudioComponent* SpeakerAudio = TheatreSpeaker->GetAudioComponent();
+        CurrentFilmReel->Film->SetRate(1);
+        SpeakerAudio->Play(CurrentFilmReel->Film->GetTime().GetTotalSeconds());
 
-		//FTimespan currentTime = CurrentFilmReel->Film->GetTime();
-		//SpeakerAudio->Stop();
-		//UE_LOG(LogTemp, Warning, TEXT("timespan Seconds: %f"), currentTime.GetSeconds());
-		//SpeakerAudio->Play(currentTime.GetTotalSeconds());
-	}
+        //FTimespan currentTime = CurrentFilmReel->Film->GetTime();
+        //SpeakerAudio->Stop();
+        //UE_LOG(LogTemp, Warning, TEXT("timespan Seconds: %f"), currentTime.GetSeconds());
+        //SpeakerAudio->Play(currentTime.GetTotalSeconds());
+    }
 }
